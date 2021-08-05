8df662e5f28cdeac852a20fcbadc6f1449a6f3d4
lmladi00 <lmladi00@fesb.hr>
2021-08-05 02:54:15 +0200

    adding headerFooter

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,13 @@
 //import; dodavanje komponenti za prikaz na stranici
-
+/*
 import React from "react"
 
 import NavigationHeader from '../components/NavigationHeader'
 
 import Footer from '../components/Footer'
-
+*/
 //NavigationHeader tag (moduralan) se mijenja sa kodom iz foldera u komponentama
-
+/*
 const IndexPage = () => (
   <main>
     <NavigationHeader />
@@ -16,3 +16,19 @@ const IndexPage = () => (
 )
 
 export default IndexPage
+*/
+
+/* NOVI PRISTUP JER SMO DODALI HEADERFOOTER*/
+
+import React from "react"
+ 
+import HeaderFooterLayout from "../layouts/headerFooter"
+ 
+const IndexPage = () => (
+  <HeaderFooterLayout activeTab="Home">
+    {/* Body components here! */}
+    </HeaderFooterLayout>
+    )
+     
+    export default IndexPage
+    
