8e4fd046b697ee6de155c36d294427b672e42bb4
lmladi00 <lmladi00@fesb.hr>
2021-08-11 17:55:24 +0200

    izbornik hover

--- src/components/Izbornik/index.js
+++ src/components/Izbornik/index.js
@@ -1,9 +1,19 @@
-import React from 'react'
-
-import * as styles from './style.module.css' 
-
-const Izbornik = () =>
- <span className={styles.izbornik}>izbornik</span>
-
+import React, { useState } from 'react'
+ 
+import * as styles from './style.module.css'
+ 
+const Izbornik = () => {
+  const [hover, setHover] = useState(false)
+ 
+  return (<section className={styles.izbornik}>
+    <article className={hover ? styles.hovering : ''} onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>
+      <div />
+      <div />
+      <div />
+    </article>
+    <span>Izbornik</span>
+  </section>
+  )
+}
+ 
 export default Izbornik
-
