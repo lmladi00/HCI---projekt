f690e2a0de20a3672d823ba751873d5c348a5888
lmladi00 <lmladi00@fesb.hr>
2021-08-24 19:09:25 +0200

    position:fixed + right:0px 3elements

--- src/components/TitleImage/index.js
+++ src/components/TitleImage/index.js
@@ -13,11 +13,13 @@ const TitleImage = () => (
     </div>
     
     <section className={styles.elements}>
+     
     <a href="#questSection"><button1 id="pitanja">?</button1></a>
 
     <button2><img src={messageimg} alt="description"/></button2>
    
     <a href="#mark"> <button3>!</button3></a>
+  
     </section>
 
   </section>
