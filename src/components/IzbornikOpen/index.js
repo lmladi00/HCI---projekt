d5b80d1c7189ccdacd5a2712228c496bef167a42
lmladi00 <lmladi00@fesb.hr>
2021-08-25 10:43:05 +0200

    IzbornikOpen<li><a>

--- src/components/IzbornikOpen/index.js
+++ src/components/IzbornikOpen/index.js
@@ -17,13 +17,13 @@ return(<section data-aos="fade-left"className={styles.izbornikopen}>
     <ImgIzbornik img="okvir1.jpg" />
         <button>X</button>
         <div className={styles.blogkateg}>
-        <a href="blog"><li>BLOG</li></a>
+        <li><a href="blog">BLOG</a></li>
             <li onClick ={() => setImage("cart.png")}>KATEGORIJE</li>
            
             <div className={styles.kateg}>   
                 <li>DIPLOMA</li>
                 <li>KRŠTENJE</li>
-                <a href="rodendan"><li>ROĐENDAN</li></a>
+                <li><a href="rodendan">ROĐENDAN</a></li>
                 <li>VALENTINOVO</li>
                 <li>VJENČANJE</li>
             </div>
