fb6888b12718e89745ff5b0f8cbab3c0bc7040d9
lmladi00 <lmladi00@fesb.hr>
2021-08-25 21:39:53 +0200

    IzbornikOpen, hover na KATEGORIJE

--- src/components/IzbornikOpen/index.js
+++ src/components/IzbornikOpen/index.js
@@ -1,36 +1,46 @@
-import React, { useState, useEffect} from "react"
+import React, { useState, useEffect } from "react"
 import * as styles from './style.module.css'
+import { navigate } from 'gatsby'
 
 import ImgIzbornik from './ImgIzbornik';
 
 import "aos/dist/aos.css";
 import Aos from "aos"
 
-const IzbornikOpen = () =>{
-  useEffect(() => {
-    Aos.init({ duration: 3000 });
-}, []);
+const IzbornikOpen = () => {
+    const [hover, setHover] = useState(false)
 
-const [img, setImage] = useState("okvir1.jpg");
-return(<section data-aos="fade-left"className={styles.izbornikopen}>
-    <div className={styles.meni}>
-    <ImgIzbornik img="okvir1.jpg" />
-        <button>X</button>
-        <div className={styles.blogkateg}>
-        <li><a href="blog">BLOG</a></li>
-            <li onClick ={() => setImage("cart.png")}>KATEGORIJE</li>
-           
-            <div className={styles.kateg}>   
-                <li>DIPLOMA</li>
-                <li>KRŠTENJE</li>
-                <li><a href="rodendan">ROĐENDAN</a></li>
-                <li>VALENTINOVO</li>
-                <li>VJENČANJE</li>
+    useEffect(() => {
+        Aos.init({ duration: 3000 });
+    }, []);
+
+    const onHovering = (e, v) => {
+        setHover(v)
+    }
+
+    const [img, setImage] = useState("okvir1.jpg");
+    return (<section data-aos="fade-left" className={styles.izbornikopen}>
+        <div className={styles.meni}>
+            <ImgIzbornik img="okvir1.jpg" />
+            <button>X</button>
+            <div className={styles.blogkateg} >
+                <li><a href="blog">BLOG</a></li>
+                <p style={{ margin: 0 }} onMouseEnter={e => onHovering(e, true)} onMouseLeave={e => onHovering(e, false)} >
+
+                    <li onClick={() => setImage("cart.png")}>KATEGORIJE</li>
+
+                    {hover && <div className={styles.kateg} >
+                        <li>DIPLOMA</li>
+                        <li>KRŠTENJE</li>
+                        <li onClick={() => navigate('rodendan')}>ROĐENDAN</li>
+                        <li>VALENTINOVO</li>
+                        <li>VJENČANJE</li>
+                    </div>}
+                </p>
+
             </div>
-
         </div>
-    </div>
-</section>
-) 
+    </section >
+    )
 }
 export default IzbornikOpen;
