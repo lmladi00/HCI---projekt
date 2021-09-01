0b5bf953663c540e396b95174953a9ab24a36d58
lmladi00 <lmladi00@fesb.hr>
2021-09-01 16:48:26 +0200

    Adding Elements to Blog

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,10 +1,11 @@
 import React from "react"
 import HeaderFooterLayout from "../layouts/headerFooter"
 import BlogForm from "../modules/BlogForm"
+import Elements from "../components/Elements"
 
 const RodendanPage = () => (
     <HeaderFooterLayout activeTab="Izbornik">
-   
+        <Elements/>
         <BlogForm />
     </HeaderFooterLayout>
 )
--- src/pages/rodendan.js
+++ src/pages/rodendan.js
@@ -1,8 +1,8 @@
 import React from "react"
 import HeaderFooterLayout from "../layouts/headerFooter"
 import RodendanForm from "../modules/RodendanForm"
-
 import Elements from "../components/Elements"
+
 const RodendanPage = () => (
     <HeaderFooterLayout activeTab="Izbornik">
         <Elements/>
