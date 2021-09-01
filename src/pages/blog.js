60a65378359c3ec6d67e84e9f7d93f1a6804ae1b
lmladi00 <lmladi00@fesb.hr>
2021-09-01 16:56:15 +0200

    blog & rodendan page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,11 +2,13 @@ import React from "react"
 import HeaderFooterLayout from "../layouts/headerFooter"
 import BlogForm from "../modules/BlogForm"
 import Elements from "../components/Elements"
+import Question from "../components/Question"
 
 const RodendanPage = () => (
     <HeaderFooterLayout activeTab="Izbornik">
         <Elements/>
         <BlogForm />
+        <Question />
     </HeaderFooterLayout>
 )
  
--- src/pages/rodendan.js
+++ src/pages/rodendan.js
@@ -2,11 +2,17 @@ import React from "react"
 import HeaderFooterLayout from "../layouts/headerFooter"
 import RodendanForm from "../modules/RodendanForm"
 import Elements from "../components/Elements"
+import Question from "../components/Question"
+import InfoMark from "../components/InfoMark"
+import Information from "../components/Information"
 
 const RodendanPage = () => (
     <HeaderFooterLayout activeTab="Izbornik">
         <Elements/>
         <RodendanForm />
+        <InfoMark />
+        <Information />
+        <Question />
     </HeaderFooterLayout>
 )
  
