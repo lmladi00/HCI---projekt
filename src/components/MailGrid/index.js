5ebd5c3073668e8ed693e2d0cba954e23409cadb
lmladi00 <lmladi00@fesb.hr>
2021-09-04 08:59:30 +0200

    MaiGrid start

--- src/components/MailGrid/index.js
+++ src/components/MailGrid/index.js
@@ -4,7 +4,16 @@ import * as styles from './style.module.css';
 
 const MailGrid = () => (
     <section  className={styles.mailGrid}>
-       
+       <form>
+           
+                    <input name="name" type="text" placeholder="Ime i prezime" />
+                      
+                    <label htmlFor="email"></label>
+                    <input name="email" type="text"placeholder="Email adresa"/>
+
+                    <button>Send a message</button>
+            
+        </form>
     </section>
    
 )
